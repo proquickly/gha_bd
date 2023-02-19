0cef0cf98dde895b3917aa6a602d55bf1d49efa0
Andy Miles <akmiles@icloud.com>
2023-02-19 10:51:01 -0800

    iedited desc

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="timechecker",
     version="0.0.1",
-    author="Edidiong Etuk",
+    author="copied from Edidiong Etuk",
     author_email="edeediong@gmail.com",
     url="https://bit.ly/edeediong-resume",
-    description="An application that informs you of the time in different locations and timezones",
+    description="timezones",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
